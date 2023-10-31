622049796b20a362f30b7180c1d2b2272ceecceb
JacopoTei <jacopotei934@gmail.com>
2023-10-31 18:27:13 +0100

    fine header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,23 +1,19 @@
- <div class="container-fluid mt-5 mb-5">
-        <div class="row justify-content-evenly altezza-h-home">
-            <div class="col-12 col-md-3 my-5">
-                <div class="position-relative img-header-sx ">
-                    <img class= "position-absolute top-50 start-50" src="https://picsum.photos/300/200" alt="">
-                    <img  src="https://picsum.photos/301/200" alt="">
-                </div>
-             </div>
-
-            <div class ="col-12 col-md-3 ">
-            <h1 class="text-center">Benvenuti nel nostro sito</h1>
-            <h3 class="text-center">Qui potrai trovare le ultime offerte</h3>
-            </div>
-
-            <div class="col-12 col-md-3 my-5">
-                <div class="position relative ">
-                    <img src="https://picsum.photos/300/200" alt="">
-                    <img class="img-header-dx" src="https://picsum.photos/301/200" alt="">
-                </div>
-             </div>
-           
+<header>
+  <!-- Background image -->
+  <div
+    class="p-5 text-center bg-image"
+  >
+    <div class="mask bg-shadow">
+      <div class="d-flex justify-content-center align-items-center h-100">
+        <div class="text-white">
+          <h1 class="mb-3">Heading</h1>
+          <h4 class="mb-3">Subheading</h4>
+          <a class="btn btn-outline-light btn-lg" href="#!" role="button"
+          >Call to action</a
+          >
         </div>
+      </div>
     </div>
+  </div>
+  <!-- Background image -->
+</header>
