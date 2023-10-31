f781e129babbaf11ad6ae23e2387831bc96345c1
Leonardo Garofalo <leone.garofalo@gmail.com>
2023-10-31 15:56:14 +0100

    Creazione componente card

---
+++ resources/views/livewire/create-card.blade.php
@@ -0,0 +1,14 @@
+<div>
+    <div class="container-fluid">
+        <h1 class="text-center">Aggiungi il tuo annuncio</h3>
+        <div class="row justify-content-evenly">
+            <div class="col-12 col-md-6 col-lg-3">
+                <div class="card my-4 shadow">
+                    <img src="https://via.placeholder.com/150" class="card-img-top" alt="Immagine">
+                    <div class="card-body">
+                        <h5 class="card-title">Titolo della Card</h5>
+                        <p class="card-text">Questo è un esempio di testo all'interno di una card Bootstrap. Puoi aggiungere ulteriori contenuti qui.</p>
+                    </div>
+                </div>
+            </div>
+</div>
