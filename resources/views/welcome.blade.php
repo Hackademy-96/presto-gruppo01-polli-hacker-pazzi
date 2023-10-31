f1985e2f30c73202d46e61654e94ba0508e4c1fb
JacopoTei <jacopotei934@gmail.com>
2023-10-31 16:26:51 +0100

    fine navbar inizio header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,5 @@
 <x-layout>
-    <div class="container">
-        <div class="row justify-content-evenly">
-
-            <div class="col-12 background ">
-            <h1 class="text-center  ">Benvenuto</h1>
-            <h3 class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
-            </div>
-           
-        </div>
-    </div>
+   
 </x-layout>
 
         
