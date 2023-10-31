2077ee468e54fd5a21c1a7449f25e24db896dc17
JacopoTei <jacopotei934@gmail.com>
2023-10-31 17:24:42 +0100

    fine header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,62 +1,23 @@
-<!-- about -->
-<div class="about">
-   <a class="bg_links social portfolio" href="https://www.rafaelalucas.com" target="_blank">
-      <span class="icon"></span>
-   </a>
-   <a class="bg_links social dribbble" href="https://dribbble.com/rafaelalucas" target="_blank">
-      <span class="icon"></span>
-   </a>
-   <a class="bg_links social linkedin" href="https://www.linkedin.com/in/rafaelalucas/" target="_blank">
-      <span class="icon"></span>
-   </a>
-   <a class="bg_links logo"></a>
-</div>
-<!-- end about -->
+ <div class="container-fluid mt-5 mb-5">
+        <div class="row justify-content-evenly altezza-h-home">
+            <div class="col-12 col-md-3 my-5">
+                <div class="position-relative img-header-sx ">
+                    <img class= "position-absolute top-50 start-50" src="https://picsum.photos/300/200" alt="">
+                    <img  src="https://picsum.photos/301/200" alt="">
+                </div>
+             </div>
 
-<nav class="mainNav">
-   <div class="mainNav__logo">Logo</div>
-   <div class="mainNav__links">
-      <a href="" class="mainNav__link">About</a>
-      <a href="" class="mainNav__link">Projects</a>
-      <a href="" class="mainNav__link">Team</a>
-      <a href="" class="mainNav__link">Contacts</a>
-   </div>
-   <div class="mainNav__icon">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-         <g data-name="Layer 2" fill="#9197AE">
-            <g data-name="menu-2">
-               <rect
-                  width="24"
-                  height="24"
-                  transform="rotate(180 12 12)"
-                  opacity="0"
-               />
-               <circle cx="4" cy="12" r="1" />
-               <rect x="7" y="11" width="14" height="2" rx=".94" ry=".94" />
-               <rect x="3" y="16" width="18" height="2" rx=".94" ry=".94" />
-               <rect x="3" y="6" width="18" height="2" rx=".94" ry=".94" />
-            </g>
-         </g>
-      </svg>
-   </div>
-</nav>
-<header class="mainHeading">
-   <div class="mainHeading__content">
-      <article class="mainHeading__text">
-         <p class="mainHeading__preTitle">nature</p>
-         <h2 class="mainHeading__title">behind the mountains</h2>
-         <p class="mainHeading__description">
-            Far far away, behind the word mountains, far from the countries
-            Vokalia and Consonantia, there live the blind texts.
-         </p>
-         <button class="cta">know more</button>
-      </article>
+            <div class ="col-12 col-md-3 ">
+            <h1 class="text-center">Benvenuti nel nostro sito</h1>
+            <h3 class="text-center">Qui potrai trovare le ultime offerte</h3>
+            </div>
 
-      <figure class="mainHeading__image">
-         <img
-            src="https://images.unsplash.com/photo-1520856707909-75c4048cc858?ixlib=rb-1.2.1&auto=format&fit=crop&w=1534&q=80"
-            alt=""
-         />
-      </figure>
-   </div>
-</header>
+            <div class="col-12 col-md-3 my-5">
+                <div class="position relative ">
+                    <img src="https://picsum.photos/300/200" alt="">
+                    <img class="img-header-dx" src="https://picsum.photos/301/200" alt="">
+                </div>
+             </div>
+           
+        </div>
+    </div>
