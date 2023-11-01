94963f0d9f04b58c1fd00c51e13189ea9fc45a6c
Megga <megga.s.box@gmail.com>
2023-11-01 17:55:22 +0100

    login e register done

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light shadow sticky-top">
-        <a class="navbar-brand" href="#">
+        <a class="navbar-brand" href="/">
             <i class="fas fa-home colorW" ></i> Home
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -25,10 +25,25 @@
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="{{route('login')}}">
-                    <i class="fas fa-sign-in-alt"></i> Login
+                    <i class="fas fa-sign-in-alt"></i> Accedi
                     </a>
                 </li>
                 @endguest
+                @auth
+                <li class="nav-item">
+                    <form method="POST" action="/logout">
+                        @csrf
+                        <li class="nav-item">
+                            <a type="submit navlink">
+                                <i class="fas fa-sign-in-alt"></i> Esci
+                            </a>
+                        </li>
+
+
+                    </form>
+                </li>
+                <a class="nav-link fw-bold colorS" href=""> Ciao {{Auth::user()->name}}!</a>
+                @endauth
             </ul>
         </div>
     </nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,5 @@
 <x-layout>
+    <x-header></x-header>
    
 </x-layout>
 
