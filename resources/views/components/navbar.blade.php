23f6bb1ca81a63c73e2b2a9b7e6968829861e14a
Leonardo Garofalo <leone.garofalo@gmail.com>
2023-11-02 17:44:12 +0100

    Inizio User story 2

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light shadow sticky-top text-white">
     <a class="navbar-brand text-white" href="{{route('welcome')}}">
-        <i class="fas fa-home colorW" ></i> Home
+        <i class="fas fa-home colorW ms-3"></i> Home
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -8,30 +8,30 @@
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ml-auto">
             <li class="nav-item">
-                <a class="nav-link text-white" href="#">
+                <a class="nav-link text-white ms-3" href="#">
                     <i class="fas fa-info-circle" ></i> Info
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-white" href="#">
+                <a class="nav-link text-white ms-3" href="#">
                     <i class="fas fa-envelope" ></i> Contatti
                 </a>
             </li>
             @guest
             <li class="nav-item">
-                <a class="nav-link text-white" href="{{route('register')}}">
+                <a class="nav-link text-white ms-3" href="{{route('register')}}">
                     <i class="fas fa-user-plus"></i> Registrati
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-white" href="{{route('login')}}">
+                <a class="nav-link text-white ms-3" href="{{route('login')}}">
                     <i class="fas fa-sign-in-alt"></i> Accedi
                 </a>
             </li>
             @endguest
             @auth
             <li class="nav-item">
-                <a class="nav-link text-white" href="{{route('article_create')}}">
+                <a class="nav-link text-white ms-3" href="{{route('article_create')}}">
                     <i class="fas fa-sign-in-alt"></i> Crea il tuo annuncio!
                 </a>
             </li>
@@ -41,7 +41,7 @@
             <li class="dropdown-item">
            <form method="POST" action="{{route('logout')}}">
                 @csrf
-            <button class="nav-link active text-white" type="submit">Logout</button>
+            <button class="nav-link active" type="submit">Logout</button>
         </form>
         </li>
             
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,11 @@
 <x-layout>
-        @if (session('message'))
+        @if(session('message'))
             <div class="alert alert-warning alert-dismissible fade show" role="alert">
-                <strong>Holy guacamole!</strong>{{session('message')}}
+                <strong>Congratulazioni! </strong>{{session('message')}}
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
         @endif
-    <x-header>
-        
-    </x-header>
+    <x-header></x-header>
 </x-layout>
 
 
