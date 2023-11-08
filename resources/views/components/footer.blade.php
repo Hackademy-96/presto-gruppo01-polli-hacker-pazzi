1e8474825d143b1565ec04357f5cb57d37983f81
Valeria Saponaro <v.saponaro97@gmail.com>
2023-11-08 13:03:20 +0100

    risoluzione problema

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -3,7 +3,7 @@
     <!-- Grid container -->
     <div class="container-fluid p-4">
       <!-- Section: Social media -->
-      <section class="mb-4">
+      {{-- <section class="mb-4">
         <!-- Facebook -->
         <a class="btn btn-outline-light btn-floating m-1 bgD" href="#!" role="button"
           ><i class="fab fa-facebook-f text-white"></i
@@ -33,15 +33,13 @@
         <a class="btn btn-outline-light btn-floating m-1 bgD" href="#!" role="button"
           ><i class="fab fa-github text-white"></i
         ></a>
-      </section>
+      </section> --}}
       <!-- Section: Social media --> 
   
       <!-- Section: Text -->
       <section class="mb-4">
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt distinctio earum
-          repellat quaerat voluptatibus placeat nam, commodi optio pariatur est quia magnam
-          eum harum corrupti dicta, aliquam sequi voluptate quas.
+          <strong>- Polli Hacker Pazzi! -</strong>
         </p>
       </section>
       <!-- Section: Text -->
@@ -55,7 +53,7 @@
         <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
           <!-- Content -->
           <h6 class="text-uppercase fw-bold mb-4">
-            <i class="fas fa-gem me-3"></i>Company name
+            <i class="fas fa-gem me-3"></i>Presto
           </h6>
           <p>
             Here you can use rows and columns to organize your footer content. Lorem ipsum
@@ -68,19 +66,22 @@
         <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
           <!-- Links -->
           <h6 class="text-uppercase fw-bold mb-4">
-            Products
+            Team PHP
           </h6>
           <p>
-            <a href="#!" class="text-reset">Angular</a>
+            <a href="#!" class="text-reset">Margherita Bombi</a>
           </p>
           <p>
-            <a href="#!" class="text-reset">React</a>
+            <a href="#!" class="text-reset">Valeria Saponaro</a>
           </p>
           <p>
-            <a href="#!" class="text-reset">Vue</a>
+            <a href="#!" class="text-reset">Noemi Podda</a>
           </p>
           <p>
-            <a href="#!" class="text-reset">Laravel</a>
+            <a href="#!" class="text-reset">Leonardo Garofalo</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Jacopo Tei</a>
           </p>
         </div>
         <!-- Grid column -->
@@ -105,7 +106,7 @@
         <!-- Grid column -->
 
         <!-- Grid column -->
-        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+        {{-- <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
           <!-- Links -->
           <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
           <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
@@ -117,7 +118,7 @@
           <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
         </div>
         <!-- Grid column -->
-      </div>
+      </div> --}}
       <!-- Grid row -->
     </div>
   </section>
