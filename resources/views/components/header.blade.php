94d87e0743b6e132cfc984b5cc79b78475230677
JacopoTei <jacopotei934@gmail.com>
2023-11-09 18:14:53 +0100

    modificato logo headewr

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -3,10 +3,10 @@
   <div
     class="p-5 text-center bg-image"
 >
-    <div class="mask bg-shadow">
+    <div class="mask bg-shadow mt-3">
       <div class="d-flex justify-content-center align-items-center h-100">
-        <div class="text-white">
-          <h1 class="mb-3">Presto</h1>
+        <div class="text-white ">
+          <img src="https://companieslogo.com/img/orig/PRST_BIG.D-3cd298b6.png?t=1687683317" width=500px alt="">
           <h4 class="mb-3">Compra, Vendi, Presto Presto!</h4>
           <a class="btn btn-outline-light btn-lg" href="{{route('article_index')}}" role="button"
           >Vai agli articoli</a>
