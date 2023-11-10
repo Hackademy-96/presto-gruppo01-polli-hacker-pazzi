3528a238283c964b1b46568618760744ef500c89
Valeria Saponaro <v.saponaro97@gmail.com>
2023-11-10 16:52:05 +0100

    en\es

--- lang/es/ui.php
+++ lang/es/ui.php
@@ -1,11 +1,18 @@
 <?php
 
 return[
-
-    // ? welcome
-
     // ? navbar
-    'categorie' =>'#Categorías',
+    'categorie' =>'Categorías',
+    '#Abbigliamento' => '#Vestimenta',
+    '#Animali' => '#Animales',
+    '#Antiquariato' => '#Antigüedades',
+    '#Arredamento' => '#Mobiliario',
+    '#Elettronica' => '#Electrónica',
+    '#Giocattoli' => '#Juguetes',
+    '#Libri & Manga' => '#Libros & Manga',
+    '#Motori' => '#Motores',
+    '#Musica' => '#Música',
+    '#Nerdzone' => '#Nerdzone',
     'registrati' => 'Regístrate',
     'accedi' => 'Accede',
     'tuttiArticoli' => 'Todos los artículos',
@@ -15,26 +22,129 @@ return[
     'saluto' => 'Hola',
     'logOut' => 'Cerrar la sesión',
     'cerca' => 'Busca',
+  
     //? fine navbar
-
+    
     // ? header
     'slogan'=>'Compra,Vende Presto Presto',
     'vaiArticoli' => 'Ir a los artículos',
     'o' => 'O',
     'caricaArticoli' => 'Cargar un artículo',
     // ? fine header
+    
+    // ? welcome
+    'weArticoli' =>'Aquí están los últimos artículos:',
+    'weDett' =>'Ir al detalle',
+    'wePill' =>'Nuevo',
+    // ? fine welcome
 
     // ? footer
     'nome' => '- ¡Pollos hackers locos! -',
-    'footerDescr' => '¡Somos un grupo de desarrolladores web junior y hemos desarrollado este sitio desde cero, desde el back-end hasta el front-end, esperamos que disfruten de su estancia!',
+    'footerDesc' => '¡Somos un grupo de desarrolladores web junior y hemos desarrollado este sitio desde cero, desde el back-end hasta el front-end, esperamos que disfruten de su estancia!',
     'team' => '¡Conoce al Equipo!',
     'teamWork' => 'Trabaja con nosotros',
     'rev' => '¡Conviértete en Revisor!',
     // ? fine footer
 
     // ? balloons
-
     'ballcat' => 'Nuestras categorías',
+    'paragrafBall' => 'Esta es la sección dedicada a las categorías de productos a la venta en nuestro sitio',
+    'paraBall' => '¡Haga clic para echar un vistazo!',
+    'bottBall' => 'Descubre más',
+    'cat1Ball' => 'Vestimenta',
+    'cat2Ball' => 'Animales',
+    'cat3Ball' => 'Antigüedades',
+    'cat4Ball' => 'Mobiliario',
+    'cat5Ball' => 'Electrónica',
+    'cat6Ball' => 'Juguetes',
+    'cat7Ball' => 'Libros & Manga',
+    'cat8Ball' => 'Motores',
+    'cat9Ball' => 'Música',
+    'cat10Ball' => 'Nerdzone',
+    // ? fine balloons
+
+    // ? show
+    'showTitolo' => 'Estás mirando',
+    'showData' => 'Subida el :',
+    'showDa' => 'Por :',
+    'showDesc' => 'Descripción :',
+    'showPrez' => 'Precio :',
+    // ? fine show
+
+    // ? categoryShow
+    'catTitolo' => 'Ver todos los artículos en',
+
+    // * 'weDett' =>'Ir al detalle',
+    // * categorie dalla navbar
+
+    // ? fine categoryShow
+
+    // ? revindex
+    'rev1' => 'Aquí está el anuncio a revisar.',
+    'rev2' => 'No hay anuncios que revisar.',
+    'revsi' => 'Acepta',
+    'revno' => 'Rechaza',
+     // ? fine revindex
+
+     // ? create-form
+    // * 'aggArticoli' => '¡Crea tu propio artículo!',
+    'creTitolo' => 'Título',
+    'crePlace' => 'Añade la descripción de tu artículo',
+    'creDesc' => 'Descripción',
+    'crePrez' => 'Precio',
+    // * categorie dalla navbar
+    'creBot1' => 'Cancela',
+    'creBot2' => 'Cargar',
+    // ? fine create-form
+
+    // ? register
+    'reName' => 'Nombre de usuario',
+    'reConfirm' => 'Confirmar Password',
+    // * 'registrati' => 'Regístrate',
+    'reBot' => 'Envía',
+    // ? fine register
+
+    // ? login
+    'logTitolo' => '¡Bienvenido!', 
+    'logParaf' => 'Si no tienes una cuenta', 
+    // * 'registrati' => 'Regístrate',
+    // * 'accedi' => 'Accede',
+    // ? fine login
+
+    // ? formrevisor
+    'revTitolo' => '¿Quieres trabajar con nosotros?',
+    // * 'rev' => '¡Conviértete en Revisor!',
+    'revSlogan' => 'Revise, acepte o rechace artículos y ayúdenos a mejorar nuestro sitio',
+    'revSlogan2' => 'Rellena el formulario y envía tu candidatura',
+    //  * 'reName' => 'Nombre de usuario',
+    // 'reBot' => 'Envía',
+    'reMes' => 'Escribe un mensaje',
+      // ? fine formrevisor
+
+      // ? index
+
+    // *  'tuttiArticoli' => 'Todos los artículos',
+     // * 'weDett' =>'Ir al detalle',
+
+     // ? fine index
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
