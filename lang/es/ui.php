f46ed9cc8cff5f2c63a45269691411ae2d7bc446
Valeria Saponaro <v.saponaro97@gmail.com>
2023-11-10 16:49:49 +0100

    spagnolo

--- lang/es/ui.php
+++ lang/es/ui.php
@@ -1,5 +1,46 @@
 <?php
 
 return[
-    'slogan'=>'Compra,Vende Presto Presto'
+
+    // ? welcome
+
+    // ? navbar
+    'categorie' =>'#Categorías',
+    'registrati' => 'Regístrate',
+    'accedi' => 'Accede',
+    'tuttiArticoli' => 'Todos los artículos',
+    'aggArticoli' => '¡Crea tu propio artículo!',
+    'messRev' => 'Mensajes no leídos',
+    'revisorZone' => 'RevisorZone',
+    'saluto' => 'Hola',
+    'logOut' => 'Cerrar la sesión',
+    'cerca' => 'Busca',
+    //? fine navbar
+
+    // ? header
+    'slogan'=>'Compra,Vende Presto Presto',
+    'vaiArticoli' => 'Ir a los artículos',
+    'o' => 'O',
+    'caricaArticoli' => 'Cargar un artículo',
+    // ? fine header
+
+    // ? footer
+    'nome' => '- ¡Pollos hackers locos! -',
+    'footerDescr' => '¡Somos un grupo de desarrolladores web junior y hemos desarrollado este sitio desde cero, desde el back-end hasta el front-end, esperamos que disfruten de su estancia!',
+    'team' => '¡Conoce al Equipo!',
+    'teamWork' => 'Trabaja con nosotros',
+    'rev' => '¡Conviértete en Revisor!',
+    // ? fine footer
+
+    // ? balloons
+
+    'ballcat' => 'Nuestras categorías',
+
+
+
+
+
+
+
+
 ];
