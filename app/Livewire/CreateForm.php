e3f769d471b68d88e6402635395fcaddde7dec99
Valeria Saponaro <v.saponaro97@gmail.com>
2023-11-13 22:28:45 +0100

    logica funzionante

--- app/Livewire/CreateForm.php
+++ app/Livewire/CreateForm.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use App\Jobs\GoogleVisionSafeSearch;
 use App\Models\Article;
 use Livewire\Component;
 use App\Models\Category;
@@ -83,6 +84,7 @@ class CreateForm extends Component
                     $newImage = $article->images()->create(['path'=>$image->store($newFileName,'public')]);
                     
                     dispatch(new ResizeImage($newImage->path, 300, 300),);
+                    dispatch(new GoogleVisionSafeSearch($newImage->id));
                 }
 
                 File::deleteDirectory(storage_path('/app/livewire-tmp'));
